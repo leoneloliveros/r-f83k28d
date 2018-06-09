5402a2940e9411160d07ef153da0813f50a1948d
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-06-09 01:34:16 -0500

    L

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.increment = this.increment.bind(this);
+
+    this.state = { value: 0 }
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.value}</span>
+        <button id="inc" onClick={this.increment}>Incrementa</button>
       </div>
     );
+  }
+
+  increment() {
+    this.setState({ value: this.state.value + 1 });
   }
 }
 
